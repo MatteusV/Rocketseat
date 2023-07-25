50c0b9590cbdfb376e5a4aa8f44f6c71a6d97d4a
Matteus Varlesse <varlesse04@gmail.com>
2023-07-25 15:01:51 -0300

    att

--- Ignite/Desafios/React/Coffee Delivery/back-end-CoffeeDelivery/src/routes/coffee.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import { knex } from '../database'
-
-export async function coffeeRoutes(app: FastifyInstance) {
-  app.get('/coffee', async (req, res) => {
-    const coffee = await knex.select('*').from('coffee')
-    return coffee
-  })
-}
